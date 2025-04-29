1cb5d84cde4f4291aa3dd2db6b80854eacf72573
Mujtaba Hassan <mujhassan786@gmail.com>
2025-04-29 05:57:30 +0300

    chore:update build docker sh file

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+
 echo "Starting to build the docker images..."
 
-echo "building project-auth:dev..."
-docker build -f auth/Dockerfile -t project-auth:dev auth/
-echo "project-auth:dev DONE"
+echo "building swarmstack-cloud-auth:latest..."
+docker build -f auth/Dockerfile -t swarmstack-cloud-auth:latest auth/
+echo "swarmstack-cloud-auth:latest DONE"
 
-echo "building project-backend:dev..."
-docker build -f backend/Dockerfile -t project-backend:dev backend/
-echo "project-backend:dev DONE"
+echo "building swarmstack-cloud-backend:latest..."
+docker build -f backend/Dockerfile -t swarmstack-cloud-backend:latest backend/
+echo "swarmstack-cloud-backend:latest DONE"
 
-echo "building project-ui:dev..."
-docker build -f frontend/Dockerfile -t project-ui:dev frontend/
-echo "project-ui:dev DONE"
+echo "building swarmstack-cloud-frontend:latest..."
+docker build -f frontend/Dockerfile -t swarmstack-cloud-frontend:latest frontend/
+echo "swarmstack-cloud-frontend:latest DONE"
+
+echo "building swarmstack-cloud-ai-microservice:latest..."
+docker build -f ai-microservice/Dockerfile -t swarmstack-cloud-ai-microservice:latest ai-microservice/
+echo "swarmstack-cloud-ai-microservice:latest DONE"
+
+echo "All docker images built successfully!"
